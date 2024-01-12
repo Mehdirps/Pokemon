1d45591321e59148f6c28724249fbfa5b22b6a8e
Mehdirps <mehdi.raposo77@gmail.com>
2024-01-12 09:57:37 +0100

    Loader update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 const Loader = (props) => {
 
     return (
-        <section className="loader" style={{ backgroundImage: `url(./img/loader-background/${props.background})` }}>
+        /* <section className="loader" style={{ backgroundImage: `url(./img/loader-background/${props.background})` }}>*/
+        <section className="loader">
             <div className="container">
                 <p>Chargement</p>
                 <div className="icons-container">
                     <figure>
-                        <img src={`./img/icons/pokemons/${props.pokemonOne}`} alt={`Illustration du pokemon ${props.pokemonOne.split('.')[0]}`} />
+                        <img src={`./img/icons/pokemons/${props.pokemonOne}`}
+                             alt={`Illustration du pokemon ${props.pokemonOne.split('.')[0]}`}/>
                     </figure>
                     <figure>
-                        <img src={`./img/icons/pokemons/${props.pokemonTwo}`} alt={`Illustration du pokemon ${props.pokemonTwo.split('.')[0]}`} />
+                        <img src={`./img/icons/pokemons/${props.pokemonTwo}`}
+                             alt={`Illustration du pokemon ${props.pokemonTwo.split('.')[0]}`}/>
                     </figure>
                     <figure>
-                        <img src={`./img/icons/pokemons/${props.pokemonThree}`} alt={`Illustration du pokemon ${props.pokemonThree.split('.')[0]}`} />
+                        <img src={`./img/icons/pokemons/${props.pokemonThree}`}
+                             alt={`Illustration du pokemon ${props.pokemonThree.split('.')[0]}`}/>
                     </figure>
                 </div>
             </div>

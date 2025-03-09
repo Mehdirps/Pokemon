67185d1c1b24294454591d6a59d41a5215764742
Mehdirps <mehdi.raposo77@gmail.com>
2025-03-09 12:36:23 +0100

    🎨 new text

--- src/pages/Dialogue.jsx
+++ src/pages/Dialogue.jsx
@@ -7,11 +7,11 @@ const Dialogue = () => {
     const navigate = useNavigate();
 
     const dialogueMessage = [
-        "  Bonjour ! Je m’appelle Mehdi Raposo Développeur FullStack Javascript sur une stack MERN. Passionné d'informatique depuis petit, j'ai en 2021 effectuer un titre professionnel RNCP bac +2 Designer Web qui m'a appris les bases du développement web en HTML - CSS - Javascript et PHP.",
-        " Suite à ma formation Designer Web, j'ai continué mon apprentissage dans le domaine en commençant une formation Titre Professionnel RNCP bac +3/4 - Développeur Web FullStack  en alternance chez Cloud Campus.",
-        " Dans le cadre de ma formation en alternance, je recherche actuellement une entreprise dans le domaine du développement web ou mobile dans l’Yonne, l’Ile-de-France ou en 100% télétravail pour poursuivre mon apprentissage.",
-        " Cette formation a pour but de me perfectionner sur des languages comme le javascript avec ses frameworks React.js et Node.js, l'approfondissement de PHP avec son framework Symfony. Durant cette formation, je vais également apprendre la gestion de projet, les méthodes de travail (agile...), la conception et modélisation",
-        " Si vous souhaitez en apprendre plus sur moi, mes compétences ou mes réalisations, je vous invite à cliquez sur le bouton ci-dessous pour suivre mon aventure !"
+        "  Bonjour ! Je m’appelle Mehdi Raposo Développeur FullStack React.js et Laravel. Passionné d'informatique depuis petit, j'ai en 2021 effectué un titre professionnel RNCP bac +2 Designer Web qui m'a appris les bases du développement web en HTML - CSS - Javascript et PHP.",
+        " Suite à ma formation Designer Web, j'ai continué mon apprentissage dans le domaine en faisant une formation Titre Professionnel RNCP bac +3/4 - Développeur Web FullStack en alternance chez Cloud Campus.",
+        " Après avoir obtenu mon bac +4, je recherche maintenant une entreprise pour poursuivre un bac +5 Chef de projet et expert en cybersécurité.",
+        " Cette formation a pour but de m'apporter des compétences en gestion de projet, en cybersécurité et en développement web pour devenir un expert dans le domaine.",
+        " Si vous souhaitez en apprendre plus sur moi, mes compétences ou mes réalisations, je vous invite à cliquer sur le bouton ci-dessous pour suivre mon aventure !"
     ]
     // TODO Ajouter toutes les phrases nécéssaires
     const NextMessage = () => {
